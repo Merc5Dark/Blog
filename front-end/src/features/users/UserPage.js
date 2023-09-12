cc44dabffee1150a1c9a93c998bf37e48984a963
Merc5Dark <31131794siya@gmail.com>
2023-09-12 10:38:21 +0200

    final touch

--- front-end/src/features/users/UserPage.js
+++ front-end/src/features/users/UserPage.js
@@ -3,17 +3,19 @@ import { useGetPostsByUserIdQuery } from '../posts/postsSlice'
 import { useGetUsersQuery } from '../users/usersSlice'
 
 const UserPage = () => {
-    
+    // Get the 'userId' parameter from the URL using React Router's 'useParams' hook
     const { userId } = useParams()
 
+    // Query to get user data
     const { user,
         isLoading: isLoadingUser,
         isSuccess: isSuccessUser,
         isError: isErrorUser,
         error: errorUser
     } = useGetUsersQuery('getUsers', {
+        // Select and map data from the query result
         selectFromResult: ({ data, isLoading, isSuccess, isError, error }) => ({
-            user: data?.entities[userId],
+            user: data?.entities[userId], // Extract the user data for the given userId
             isLoading,
             isSuccess,
             isError,
@@ -21,6 +23,7 @@ const UserPage = () => {
         }),
     })
 
+    // Query to get posts by the user
     const {
         data: postsForUser,
         isLoading,
@@ -30,14 +33,18 @@ const UserPage = () => {
     } = useGetPostsByUserIdQuery(userId);
 
     let content;
+    // Check if either of the queries is in a loading state
     if (isLoading || isLoadingUser) {
         content = <p>Loading...</p>
-    } else if (isSuccess && isSuccessUser) {
+    } 
+    // Check if both queries were successful
+    else if (isSuccess && isSuccessUser) {
         const { ids, entities } = postsForUser
         content = (
             <section>
                 <h2>{user?.name}</h2>
                 <ol>
+                    {/* Map through the post IDs and display them as links */}
                     {ids.map(id => (
                         <li key={id}>
                             <Link to={`/post/${id}`}>{entities[id].title}</Link>
@@ -46,7 +53,9 @@ const UserPage = () => {
                 </ol>
             </section>
         )
-    } else if (isError || isErrorUser) {
+    } 
+    // Check if there was an error in either of the queries
+    else if (isError || isErrorUser) {
         content = <p>{error || errorUser}</p>;
     }
 
--- front-end/src/features/users/UsersList.js
+++ front-end/src/features/users/UsersList.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import { useGetUsersQuery } from './usersSlice'
 
 const UsersList = () => {
-
+    // Query to get a list of users
     const {
         data: users,
         isLoading,
@@ -12,24 +12,29 @@ const UsersList = () => {
     } = useGetUsersQuery('getUsers')
 
     let content;
+    // Check if the query is in a loading state
     if (isLoading) {
         content = <p>"Loading..."</p>;
-    } else if (isSuccess) {
-
+    } 
+    // Check if the query was successful
+    else if (isSuccess) {
+        // Map through the list of user IDs and create links for each user
         const renderedUsers = users.ids.map(userId => (
             <li key={userId}>
                 <Link to={`/user/${userId}`}>{users.entities[userId].name}</Link>
             </li>
         ))
 
+        // Display the list of users
         content = (
             <section>
                 <h2>Users</h2>
-
                 <ul>{renderedUsers}</ul>
             </section>
         )
-    } else if (isError) {
+    } 
+    // Check if there was an error in the query
+    else if (isError) {
         content = <p>{error}</p>;
     }
 
